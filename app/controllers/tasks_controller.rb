af2f00632521bda7d0b3050ce384284e31a9c848
masahito-nagayama <me18sf67m@gmail.com>
2021-10-27 00:06:20 +0900

    add form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,8 +2,17 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
-
+  
   def show
-    @task = Task.where(params[:id])
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new(task_params)
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name, :description, :deadline)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'tasks#index'
-  
-  get 'tasks/show'
+
+  get 'tasks#show', to: 'tasks/show'
+  get 'tasks#new', to: 'tasks/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
